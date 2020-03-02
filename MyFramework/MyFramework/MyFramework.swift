e0865eb73b735465c459de154a42c59c65b9129e
John McIntosh <johntmcintosh@gmail.com>
2020-03-02 16:30:34 -0600

    Create empty framework

---
+++ MyFramework/MyFramework/MyFramework.swift
@@ -0,0 +1,17 @@
+//
+//  MyFramework.swift
+//  MyFramework
+//
+//  Created by John McIntosh on 3/2/20.
+//  Copyright © 2020 John T McIntosh. All rights reserved.
+//
+
+import Foundation
+
+public class MyFramework {
+    public init() { }
+    
+    public static func launch() {
+        print("Hello from MyFramework.")
+    }
+}
